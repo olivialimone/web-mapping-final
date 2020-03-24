3522bdf165c85b3adca921e10c996b93cc75c528
olivialimone <olivelemon@yahoo.com>
2020-03-24 14:19:33 -0400

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -3,7 +3,7 @@ mapboxgl.accessToken = 'pk.eyJ1Ijoib2xpdmlhbGltb25lIiwiYSI6ImNrNmxmOXNqNzBlZnEzZ
 
 //map centered on NYC
 var initialCenterPoint = [-73.9712, 40.7128]
-var initialZoom = 9.4
+var initialZoom = 9.9
 
 // set the default text for the feature-info div
 var defaultText = '<p>Move the mouse XYZ</p>'
@@ -26,24 +26,44 @@ map.addControl(new mapboxgl.NavigationControl());
 // wait for the initial style to Load
 map.on('style.load', function() {
 
-      // add a geojson source to the map using our external geojson file
-      map.addSource('subway-access', {
-        type: 'geojson',
-        data: './data/subway-access.geojson',
-      });
+  // add a geojson source to the map using our external geojson file
+//map.addSource('subway-access', {
+  //type: 'geojson',
+  //data: './data/subway-access.geojson',
+  //});
 
-      // add a geojson source to the map using our external geojson file
-      map.addSource('subway-lines', {
-        type: 'geojson',
-        data: './data/subway-access.geojson',
-      });
+  // add a geojson source to the map using our external geojson file
+  map.addSource('subway-lines', {
+    type: 'geojson',
+    data: './data/subway-access.geojson',
+  });
 
-      // log the current map state to the console
-      console.log(map.getStyle().sources);
+  // log the current map state to the console
+  console.log(map.getStyle().sources);
 
-      // add the subway lines and their corresponding colors
-      map.addLayer({
-            id: 'subway-lines-fill',
-            type: 'fill',
-            source: 'subway-lines',
-            paint:
+  // add the subway lines and their corresponding colors
+  map.addLayer({
+  id: 'subway-lines-color',
+  type: 'line',
+  source: 'subway-lines',
+  layout: {
+    'line-join':'round',
+    'line-cap':'round'
+  },
+    'paint':{
+      'line-color': 'subway-lines.geoJSON'.color,
+      'line-width': 5
+    }
+
+
+
+  //For each subway station, I want to add a popup with information about accessibility
+  //new mapboxgl.Marker()
+    //.setLngLat(['subway-access'.stop_lon, 'subway-access'.stop_lat])
+    //.setPopup(new mapboxgl.Popup({
+      //offset: 25
+      //}) // add popups
+      //.setHTML(`${subway-access.stop_name} "is " `))
+    //  .addTo('map-container');
+});
+})
